1789ba1eb69f7d4edf7956cc61f1783b5aebaf91
Salvatore C Ficara <ficaras0@gmail.com>
2016-04-12 12:33:04 -0400

    Update SystemConstructionPanelJList.java

--- src/SystemConstructionPanelJList.java
+++ src/SystemConstructionPanelJList.java
@@ -1,4 +1,4 @@
-package src;
+package V1;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -46,18 +46,26 @@ public class SystemConstructionPanelJList extends JPanel {
 
 		makeJList(); // Construct the JList panel
 
-		//Get references to necessary JComponents
 		JViewport consolePane = (JViewport) ((JScrollPane) conPanel.getPanel().getComponent(0)).getViewport(); 
 		consoleTextArea = (JTextPane) consolePane.getView();						
 		addRoomTextInput = (JTextField) inPanel.getComponent(0); 
+
 		listPane = (JScrollPane) this.getComponent(0); 
+
 		list = (JList<Room>) listPane.getViewport().getComponent(0); 
+	
 		listModel = (DefaultListModel<Room>) list.getModel(); 
+	
 		JPanel buttonPanel = (JPanel) this.getComponent(1); 
 		createRoomButton = (JButton) buttonPanel.getComponent(0); 
-		deleteRoomButton = (JButton) buttonPanel.getComponent(1); 
-		backButton = (JButton) buttonPanel.getComponent(3); 
-		editRoomButton = (JButton) buttonPanel.getComponent(2); 
+
+		deleteRoomButton = (JButton) buttonPanel.getComponent(1); // Reference
+
+		backButton = (JButton) buttonPanel.getComponent(3); // Reference to the
+															// back JButton
+		editRoomButton = (JButton) buttonPanel.getComponent(2); // Reference to
+																// the edit
+																// JButton
 
 		addListeners(); // Add listeners
 		loadRoomList(); // Load the room list into JList model
@@ -142,7 +150,8 @@ public class SystemConstructionPanelJList extends JPanel {
 					writeString += "<b>Created on : </b>" + "<span style=\"color:rgb(0, 137, 178);font:14px;\">"
 							+ InformationDisplay.dateOfCreationFormatted() + " at "
 							+ InformationDisplay.timeOfCreationFormatted() + "</span><br><br>";
-					PrintWriter write = new PrintWriter(new BufferedWriter(new FileWriter(currentBusiness.getFile(), true)));
+					File currentFile = currentBusiness.getFile();
+					PrintWriter write = new PrintWriter(new BufferedWriter(new FileWriter(currentFile, true)));
 					write.write("\n\n");
 					write.write(writeString);
 					write.close();
@@ -175,23 +184,55 @@ public class SystemConstructionPanelJList extends JPanel {
 	private Alarm[] createSelectedAlarmsArray() {
 		Alarm[] alarms = new Alarm[7];
 		JPanel panelArray = (JPanel) alarmsPanel.getComponent(0);
-		int i = -1;
+		int i = 0;
 		for (Component c : panelArray.getComponents()) {
 			if (c instanceof JCheckBox) {
 				if (((JCheckBox) c).isSelected()) {
 					String alarmText = ((JCheckBox) c).getText();
-					alarms[++i] = new Alarm(((JCheckBox) c).getText());
+					alarms[i++]  = checkAlarm(alarmText);
 				}
 			}
 		}
 		return alarms;
 	}
-
+	
+	//  Match the alarm or alarms with the JCheckBox by using 
+	//  the Switch statement
+	private Alarm checkAlarm(String i)
+	{
+		Alarm al = null;
+		switch(i)
+		{
+			case "No Alarm":
+				al = new NoAlarm ("No Alarm", 0);
+				break;
+			case "Chemical":
+				al = new ChemicalAlarm ("Chemical Alarm", 2);
+				break;
+			case "Fire":
+				al = new FireAlarm ("Fire Alarm", 4);
+				break;
+			case "Window":
+				al = new WindowAlarm ("Window Alarm", 1);
+				break;
+			case"Door":
+				al = new DoorAlarm ("Door Alarm", 1);
+				break;
+			case "Flood":
+				al = new FloodAlarm ("Flood Alarm", 3);
+				break;
+			case "Zombie Apocalypse":
+				al = new ZombieAlarm ("Zombie Apocalypse", 10000);
+				break;
+		}
+		
+		return al;
+	}
+	
 	// Load list of rooms from current business upon opening
 	private void loadRoomList() {
 		for (Room r : currentBusiness.getRooms()) {
 			listModel.addElement(r);
 		}
 	}
-
 }
