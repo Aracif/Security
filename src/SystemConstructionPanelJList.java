97e3ddb15107c2e193199a8fa2ee9f12ab7a1f99
Zidaine <charltondane@gmail.com>
2016-04-23 13:58:34 -0400

    Update SystemConstructionPanelJList.java

--- src/SystemConstructionPanelJList.java
+++ src/SystemConstructionPanelJList.java
@@ -1,4 +1,4 @@
-package src;
+package V1;
 
 import javax.swing.*;
 import javax.swing.border.Border;
@@ -9,7 +9,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.PrintWriter;
-import java.io.Reader;
+import java.nio.file.Files;
+import java.util.Arrays;
 
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -26,56 +27,43 @@ public class SystemConstructionPanelJList extends JPanel {
 	private JButton createRoomButton;
 	private JButton deleteRoomButton;
 	private JButton editRoomButton;
+	private JButton triggerButton;
 	private JButton backButton;
 	private JScrollPane listPane;
 	private DefaultListModel<Room> listModel;
 	private JList<Room> list;
 	private JTextField addRoomTextInput;
 	private JTextPane consoleTextArea;
-	File inputFile;
+	private File currentBusinessLogFile;
 
 	@SuppressWarnings("unchecked")
 	public SystemConstructionPanelJList(JPanel alarms, Business bus, SystemConstructionPanelInput inPanel,
-			SystemConstructionConsole conPanel, JFrame fra) {
+			SystemConstructionConsole conPanel, JFrame fra) throws IOException {
 
 		this.setLayout(new FlowLayout());
-
-		topFrame = fra; // reference to the frame for system creation so it can
-						// be closed
-		alarmsPanel = alarms; // reference to alarm panel
-		currentBusiness = bus; // current business object
-
+		topFrame = fra; 
+		alarmsPanel = alarms; 
+		triggerButton = new JButton ("Trigger Alarm");
+		alarms.add(triggerButton);		
+		currentBusiness = bus; 
 		blackline = BorderFactory.createLineBorder(Color.decode("#00BFFF")); 
-
 		this.setBorder(BorderFactory.createTitledBorder(blackline, "Room List"));//
-
-		makeJList(); // Construct the JList panel
-
+		makeJList(); 
 		JViewport consolePane = (JViewport) ((JScrollPane) conPanel.getPanel().getComponent(0)).getViewport(); 
 		consoleTextArea = (JTextPane) consolePane.getView();						
 		addRoomTextInput = (JTextField) inPanel.getComponent(0); 
-
 		listPane = (JScrollPane) this.getComponent(0); 
-
-		list = (JList<Room>) listPane.getViewport().getComponent(0); 
-	
-		listModel = (DefaultListModel<Room>) list.getModel(); 
-	
+		list = (JList<Room>) listPane.getViewport().getComponent(0); 	
+		listModel = (DefaultListModel<Room>) list.getModel(); 	
 		JPanel buttonPanel = (JPanel) this.getComponent(1); 
-		createRoomButton = (JButton) buttonPanel.getComponent(0); 
-
-		deleteRoomButton = (JButton) buttonPanel.getComponent(1); // Reference
-
-		backButton = (JButton) buttonPanel.getComponent(3); // Reference to the
-															// back JButton
-		editRoomButton = (JButton) buttonPanel.getComponent(2); // Reference to
-																// the edit
-																// JButton
+		createRoomButton = (JButton) buttonPanel.getComponent(0); 		
+		deleteRoomButton = (JButton) buttonPanel.getComponent(1); 
+		backButton = (JButton) buttonPanel.getComponent(3); 														
+		editRoomButton = (JButton) buttonPanel.getComponent(2); 
+		currentBusinessLogFile = currentBusiness.getFile();																
+		addListeners(); 
+		loadRoomList();
 		
-		inputFile = currentBusiness.getFile();
-		
-		addListeners(); // Add listeners
-		loadRoomList(); // Load the room list into JList model
 	}
 
 	private void makeJList() {
@@ -98,33 +86,39 @@ public class SystemConstructionPanelJList extends JPanel {
 		buttonPanel.add(back);
 		this.add(buttonPanel);
 	}
+	
+	// A method to write and save the log of the trigger button
+	// Not a full version but it works the way we want
+	public String wordSearch(String s, PrintWriter bw) throws IOException {
+		BufferedReader r = new BufferedReader(new FileReader(currentBusinessLogFile));
+		String currentLine;
+		String text = "";
+		String newString = "";
+		while((currentLine = r.readLine())!=null){
+			if(currentLine.contains(s)){
+				newString = currentLine.replace(s, s + " TRIGGERED" );
+				System.out.println(newString);
+				text=newString;
+				return text;
+			}
 
-	private void addListeners() {
+		}
+		System.out.println(text);
+		return currentLine;	
+	}
+
+
+	private void addListeners() throws IOException {
 		// DELETE BUTTON
 		deleteRoomButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
 				int selectedInd = list.getSelectedIndex();
 				Room currentRoom = listModel.getElementAt(selectedInd);
-				
-				try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(inputFile, true))))
-				{
-					writer.write("\n<br><br><b>*ROOM DELETION*</b>\n");
-					writer.write("<br><b>Room Name : </b>" + currentRoom.getRoomName());
-				} 
-				catch(FileNotFoundException e1)
-				{
-					e1.printStackTrace();
-				}
-				catch (IOException e1)
-				{
-					e1.printStackTrace();
-				}
-				
 				listModel.removeElementAt(selectedInd);
 				currentBusiness.getRooms().remove(currentRoom);
 			}
 		});
+			
 		// UPDATE CONSOLE THROUGH JLIST SELECTIONS
 		list.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent e) {
@@ -140,12 +134,16 @@ public class SystemConstructionPanelJList extends JPanel {
 				}
 			}
 		});
+		
+		
 		// BACK BUTTON
 		backButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				topFrame.dispose();
 			}
 		});
+		
+	
 		// EDIT ROOM BUTTON
 		editRoomButton.addActionListener(new ActionListener() {
 			
@@ -172,6 +170,70 @@ public class SystemConstructionPanelJList extends JPanel {
 			}
 			
 		});
+		
+		//TRIGGER BUTTON
+		//That included writing log to the file
+		triggerButton.addActionListener(new ActionListener()
+		{	
+			
+			PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(currentBusinessLogFile,true)));
+			
+			public void actionPerformed(ActionEvent e)
+			{
+				Alarm[] a = createSelectedAlarmsArray();
+				int j = 0;
+				for (Alarm k = a[j]; k!=null; k = a[++j])
+				{
+					try{
+					switch(k.getName())
+					{
+						case "Chemical Alarm":	
+							String s = wordSearch("Chemical Alarm", writer);							
+							writer.write(s);
+							writer.close();							
+							popUp(k.goesOff());
+							break;
+						case "Fire Alarm":
+							String t = wordSearch("Fire Alarm", writer);							
+							writer.write(t);
+							writer.close();	
+							popUp(k.goesOff());
+							break;
+						case "Window Alarm":
+							String d = wordSearch("Window Alarm", writer);							
+							writer.write(d);
+							writer.close();	
+							popUp(k.goesOff());
+							break;
+						case"Door Alarm":
+							String h = wordSearch("Door Alarm", writer);							
+							writer.write(h);
+							writer.close();	
+							popUp(k.goesOff());
+							break;
+						case "Flood Alarm":
+							String i = wordSearch("Flood Alarm", writer);							
+							writer.write(i);
+							writer.close();	
+							popUp(k.goesOff());
+							break;
+						case "Zombie Apocalypse":
+							String v = wordSearch("Zombie Alarm", writer);							
+							writer.write(v);
+							writer.close();	
+							popUp(k.goesOff());
+							break;
+					}
+					}
+					catch(IOException y){
+						y.printStackTrace();
+					}
+				}
+			}
+
+		});
+	
+	
 		// ADD ROOM BUTTON
 		createRoomButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -186,7 +248,7 @@ public class SystemConstructionPanelJList extends JPanel {
 				statusText += "<p><b>RISK LEVEL : </b>" + newRoom.getRiskLevel() + "</p>";
 				statusText += "<p><b>ALARMS : </b></p>";
 				statusText += InformationDisplay.getAlarmNames(alarms) + "\n";
-				statusText += "<b>Creation Status </b>: <span style=\"color:Green\">Success</span> ";
+				statusText += "<p><b>Creation Status </b></p>: <span style=\"color:Green\">Success</span> ";
 
 				consoleTextArea.setText("<b>Current Business : </b>" + currentBusiness.getName() + "\n" + statusText);
 
@@ -238,22 +300,23 @@ public class SystemConstructionPanelJList extends JPanel {
 			if (c instanceof JCheckBox) {
 				if (((JCheckBox) c).isSelected()) {
 					String alarmText = ((JCheckBox) c).getText();
-					alarms[i++]  = checkAlarm(alarmText);
+					alarms[i++]  = checkAlarm(alarmText);					
 				}
 			}
 		}
 		return alarms;
 	}
 	
-	// Matching the alarm with 
-	// the JCheckBox by using Switch
+	// Method [to be used in createSelectedAlarmsArray()] checking the name of the the alarms
+	// If the box/boxes [ that is/are checked ] is matching the case/cases
+	// It will create that alarm
 	private Alarm checkAlarm(String i)
 	{
 		Alarm al = null;
 		switch(i)
 		{
-			case "Drill":
-				al = new NoAlarm ("Drill", 0);
+			case "No Alarm":
+				al = new NoAlarm ("No Alarm", 0);
 				break;
 			case "Chemical":
 				al = new ChemicalAlarm ("Chemical Alarm", 2);
@@ -271,12 +334,20 @@ public class SystemConstructionPanelJList extends JPanel {
 				al = new FloodAlarm ("Flood Alarm", 3);
 				break;
 			case "Zombie Apocalypse":
-				al = new ZombieAlarm ("Zombie Apocalypse", 10000);
+				al = new ZombieAlarm ("Zombie Apocalypse", 9999);
 				break;
 		}
 		
 		return al;
 	}
+	
+	// Method [will be used in the trigger button] that passes a String
+	// Which will create a message dialog [ invoke the goesOff() method ] 
+	public void popUp(String i)
+	{
+		JOptionPane.showMessageDialog(topFrame, i);
+	}
+	
 	
 	// Load list of rooms from current business upon opening
 	private void loadRoomList() {
