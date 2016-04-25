40c8f1f9d550d97e6f3dea0151fd1640032b3cd4
dot0 <dot0@EducLab211327.rowanads.rowan.edu>
2016-04-25 17:55:12 -0400

    Added a comment

--- src/Alarm.java
+++ src/Alarm.java
@@ -4,34 +4,65 @@ import java.io.Serializable;
 
 public abstract class Alarm implements Serializable {
 	
+	// Name of the alarm
 	private String name;
+	// The level of priority of the alarm
 	private int priority;
+	
 	/**
+	 * Alarm constructor
 	 * 
-	 * @param name
-	 * @param priority
+	 * @param name of the alarm
+	 * @param priority level of the alarm
 	 */
 	public Alarm(String name, int priority) {
 		this.name = name;
 		this.priority = priority;
 	}
 
-	public String getName() {
+	
+	/**
+	 * 
+	 * @return name of the alarm
+	 */
+	public String getName() 
+	{
 		return name;
 	}
 
-	public void setName(String name) {
+	/**
+	 * 
+	 * @param name set new name for the alarm
+	 * 
+	 */
+	public void setName(String name) 
+	{
 		this.name = name;
 	}
-	
-	public void setPriority(int priority) {
-		this.priority = priority;
-	}
-	
-	public int getPriority() {
+
+	/**
+	 * 
+	 * @return level of priority 
+	 */
+	public int getPriority() 
+	{
 		return priority;
 	}
 
+	/**
+	 * 
+	 * @param priority set new priority level
+	 */
+	public void setPriority(int priority) 
+	{
+		this.priority = priority;
+	}
+	
+	/**
+	 * An abstract method need to be used by the sub-class of Alarm
+	 * @return a String contains the name of the alarm
+	 * and the level of priority
+	 */
 	public abstract String goesOff();
 
 }
